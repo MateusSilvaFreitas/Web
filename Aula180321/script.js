abdfea149c652bfbe63089b1fd8e6e3b91bcb0d0
Mateus Silva Freitas <51176161+MateusSilvaFreitas@users.noreply.github.com>
2021-03-24 23:57:18 -0300

    Update script.js

--- Aula180321/script.js
+++ Aula180321/script.js
@@ -17,28 +17,48 @@ $(function(){
                 $('.loading').remove();
             }
         })
-        
-        function mostrarDados(dados){
-            $.each(dados, function(i, el){
-                tbody.append(`<tr class="linha">
-                                <th scope="row" class="id">${el.id}</th>
-                                <td class="nome">${el.name}</td>
-                                <td class="username">${el.username}</td>
-                                <td class="email">${el.email}</td>
-                                <td class="endereco">Rua: ${el.address.street}, Apartamento: ${el.address.suite}, Código Postal: ${el.address.zipcode}, Latitude: ${el.address.geo.lat} | Longitude: ${el.address.geo.lng}
-                                    <tr>
-                                </td>
-                              </tr>                              
-             `)
-            })
-        }
     }
     obterDados();
+    
+    function mostrarDados(dados){
+        $.each(dados, function(i, el){
+            tbody.append(`<tr class="linha">
+                            <th scope="row" class="id">${el.id}</th>
+                            <td class="nome">${el.name}</td>
+                            <td class="username">${el.username}</td>
+                            <td class="email">${el.email}</td>
+                            <td class="street">Rua: ${el.street}</td>
+                          </tr>                              
+         `)
+        })
+    }
+    // , Apartamento: ${el.address.suite}, Código Postal: ${el.address.zipcode}, Latitude: ${el.address.geo.lat} | Longitude: ${el.address.geo.lng}
     tbody.on('click', (e) => {    
         $linha = $(e.target).closest(".linha");
         $("#name").val($linha.children(".nome").text());
         $("#username").val($linha.children(".username").text());
         $("#email").val($linha.children(".email").text());
-        $("#endereco").val($linha.children(".endereco").text());
+        $("#street").val($linha.children(".street").text());
+    })
+
+    $('form').on('submit', function (e){
+        e.preventDefault();
+        const dados = {};
+        $('form').find('input').each(function(i, el){
+            dados[el.id] = el.value;
+        })
+       
+        $.ajax('https://jsonplaceholder.typicode.com/users', {
+            type: 'POST',
+            data: dados,
+            success: function(_dados){
+                console.log(_dados);
+                mostrarDados([_dados]);
+                alert("SUCESSO!");
+            },
+            error: function(){
+                alert("ERRO!")
+            }
+        })
     })
 })
