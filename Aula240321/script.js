8ab0af01abe75d5a9e7034c5c25ef337670c03c0
Mateus Silva Freitas <51176161+MateusSilvaFreitas@users.noreply.github.com>
2021-03-26 21:41:13 -0300

    Update script.js

--- Aula240321/script.js
+++ Aula240321/script.js
@@ -1,9 +1,13 @@
 $(function(){
-    $("#temporada").focusout(function(e){
+    $("#botaoSubmit").on('click', function(e){
+        e.preventDefault();
+        var liga = $("#ligas").val();
+        var temporada = $("#temporada").val();
+        
         var settings = {
             "async": true,
             "crossDomain": true,
-            "url": `https://api-football-beta.p.rapidapi.com/teams?league=71&season=${this.value}`,
+            "url": `https://api-football-beta.p.rapidapi.com/teams?league=${liga}&season=${temporada}`,
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "dd1ab06470msh12c63d89fba8c10p12a625jsnf0ab35c95d4d",
@@ -14,7 +18,7 @@ $(function(){
         settings = {
             "async": true,
             "crossDomain": true,
-            "url": `https://api-football-beta.p.rapidapi.com/players/topscorers?season=${this.value}&league=71`,
+            "url": `https://api-football-beta.p.rapidapi.com/players/topscorers?season=${temporada}&league=${liga}`,
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "dd1ab06470msh12c63d89fba8c10p12a625jsnf0ab35c95d4d",
@@ -24,9 +28,34 @@ $(function(){
         fazRequisicaoMarcadores(settings);
     })
 
+    $('.dados section').on('click', function(e){
+        console.log("ENTROU")
+        var idTime = $(e.target).closest("section").attr('id');
+        var temporada = $("#temporada").val();
+        var liga = $("#ligas").val();
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": `https://api-football-beta.p.rapidapi.com/teams/statistics?team=${idTime}&season=${temporada}&league=${liga}`,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "dd1ab06470msh12c63d89fba8c10p12a625jsnf0ab35c95d4d",
+                "x-rapidapi-host": "api-football-beta.p.rapidapi.com"
+            }
+        }
+        $.ajax(settings).done(function (response){
+            alert("aa")
+            criaTelaInfoTime(response.response);
+        })
+    })
+
     function fazRequisicao(settings){
         $.ajax(settings).done(function (response){
-            criaTela(response.response);
+            if(response.response[0] === undefined){
+                alert('Não foi possível encontrar resultados para esta temporada')
+            } else {
+                criaTela(response.response);
+            }
         })
     }
     function fazRequisicaoMarcadores(settings){
@@ -36,6 +65,7 @@ $(function(){
     }
     
     function criaTela(dados){
+        $(".dados").text("");
         $.each(dados, function(i, el){
             $(".dados").append(
                 `<section id="${el.team.id}">
@@ -49,6 +79,7 @@ $(function(){
     }
     function criaTelaMarcadores(dadosMarcadores){
         count = 0;
+        $(".dadosMarcadores").text("");
         $.each(dadosMarcadores, function(i, el){
             if(count < 10){
                 $(".dadosMarcadores").append(
@@ -73,6 +104,68 @@ $(function(){
             }
             count++;
         })
+    }
+    function criaTelaInfoTime(dadosTime){
+        $(".dadosTime").text("");
+        $(".dadosTime").append(`
+    <section>
+        <header>
+            <figure> 
+                <img src="${dadosTime.league.logo}" alt="Logo ${dadosTime.league.name}">
+            </figure>
+            <h5>${dadosTime.league.country} - ${dadosTime.league.name} <img src="${dadosTime.league.flag}" alt="Logo ${dadosTime.league.country}"></h5>
+            <br>
+            <h5>${dadosTime.team.name} <img src="${dadosTime.team.logo}" alt="Logo ${dadosTime.team.name}"></h5>
+        </header>
+        <article>
+            <h5>Maiores Resultados</h5>
+            <h6>Gols feitos</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.biggest.goals.for.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.biggest.goals.for.away} </p>
+            <h6>Gols tomados</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.biggest.goals.against.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.biggest.goals.against.away} </p>
+            <h6>Vitória</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.biggest.wins.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.biggest.wins.away} </p>
+            <h6>Derrota</h6>
+              <p><span class="negrito">Em casa:</span> ${dadosTime.biggest.loses.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.biggest.loses.away} </p>
+            <h6>Sequencias Máximas</h6>
+            <p><span class="negrito">Vitórias</span> ${dadosTime.biggest.streak.wins} </p>
+            <p><span class="negrito">Empates</span> ${dadosTime.biggest.streak.draw} </p>
+            <p><span class="negrito">Derrotas</span> ${dadosTime.biggest.streak.loses} </p>
+
+            <h5>Jogos</h5>
+            <h6>Vitórias</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.fixtures.wins.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.fixtures.wins.away} </p>
+            <p><span class="negrito">Total:</span> ${dadosTime.fixtures.wins.total} </p>
+            <h6>Empate</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.fixtures.draws.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.fixtures.draws.away} </p>
+            <p><span class="negrito">Total:</span> ${dadosTime.fixtures.draws.total} </p>
+            <h6>Derrotas</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.fixtures.loses.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.fixtures.loses.away} </p>
+            <p><span class="negrito">Total:</span> ${dadosTime.fixtures.loses.total} </p>
+            <h6>Jogos jogados</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.fixtures.played.home} </p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.fixtures.played.away} </p>
+            <p><span class="negrito">Total:</span> ${dadosTime.fixtures.played.total} </p>
+            
+            <h5>Gols</h5>
+            <h6>Gols feitos</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.goals.for.total.home} (MÉDIA PORA JOGO: ${dadosTime.goals.for.average.home})</p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.goals.for.total.away} (MÉDIA PORA JOGO: ${dadosTime.goals.for.average.away})</p>
+            <p><span class="negrito">Total:</span> ${dadosTime.goals.for.total.total} (MÉDIA PORA JOGO: ${dadosTime.goals.for.average.total})</p>
+            <h6>Gols tomados</h6>
+            <p><span class="negrito">Em casa:</span> ${dadosTime.goals.against.total.home} (MÉDIA PORA JOGO: ${dadosTime.goals.against.average.home})</p>
+            <p><span class="negrito">Fora de casa:</span> ${dadosTime.goals.against.total.away} (MÉDIA PORA JOGO: ${dadosTime.goals.against.average.away})</p>
+            <p><span class="negrito">Total:</span> ${dadosTime.goals.against.total.total} (MÉDIA PORA JOGO: ${dadosTime.goals.against.average.total})</p>
+        </article>
+    </section>
+        `)
     }
 
 })
